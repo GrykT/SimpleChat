73834a2a84489e299bc4fd7fce7286f2403781eb
Hir <45359174+GrykT@users.noreply.github.com>
2019-02-02 20:40:42 +0900

    再構成

--- SimpleChat/settings.py
+++ SimpleChat/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'ja-JP'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Users,Talk
 
-@admin.register(Users)
-class UsersAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Talk)
-class TalkAdmin(admin.ModelAdmin):
-    pass
+# Register your models here.
